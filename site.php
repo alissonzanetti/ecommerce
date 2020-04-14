e00e4708d35ad724934869748077e7687f2cc53a
alissonzanetti <175477@upf.br>
2020-04-14 10:04:09 -0300

    Produto vs Categoria

--- site.php
+++ site.php
@@ -2,17 +2,27 @@
 
 use \Hcode\Page;
 use \Hcode\Model\Product;
+use \Hcode\Model\Category;
 
 $app->get('/', function() {
   $products = Product::listAll();
-
   $page = new Page();
   //Function __construct will call the header.html page
   //Function setTpl("index") will call the index.html page
   $page->setTpl("index", [
-      "products"=>Product::checkList($products) 
+      "products"=>Product::checkList($products)
   ]);
   //Function __destruct will call the footer.html page
+});
+
+$app->get("/categories/:idcategory", function($idcategory){
+  $category = new Category();
+  $category->get((int)$idcategory);
+  $page = new Page();
+  $page->setTpl("category", [
+    "category"=>$category->getValues(),
+    "products"=>Product::checklist($category->getProducts())
+  ]);
 });
 
 
